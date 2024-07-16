66797382df92d2729812eeee9b42ef2f3f22c83f
vocuc203 <nguyenthuan281294@gmail.com>
2024-07-16 11:49:51 +0700

    5/13/2024 update

--- src/main/java/com/nts/awspremium/controller/HistoryViewController.java
+++ src/main/java/com/nts/awspremium/controller/HistoryViewController.java
@@ -325,6 +325,13 @@ public class HistoryViewController {
         Random ran = new Random();
         try {
             Thread.sleep(ran.nextInt(1000));
+
+            if(ran.nextInt(100)<60){
+                resp.put("status", "fail");
+                resp.put("message", "Bỏ qua nhiệm vụ");
+                return new ResponseEntity<String>(resp.toJSONString(), HttpStatus.OK);
+            }
+
             Long historieId = historyViewRepository.getId(username);
             List<VideoView> videos = null;
             if (historieId == null) {
