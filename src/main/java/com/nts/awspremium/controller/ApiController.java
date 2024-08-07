9a19090069de4a704c6674e499ac7b3953f5906f
vocuc203 <nguyenthuan281294@gmail.com>
2024-08-07 23:47:41 +0700

    7/26/2024 update

--- src/main/java/com/nts/awspremium/controller/ApiController.java
+++ src/main/java/com/nts/awspremium/controller/ApiController.java
@@ -331,10 +331,10 @@ public class ApiController {
                         int duration_min=2+(int)(Duration.parse(contentDetails.get("duration").toString()).getSeconds()/60);
                         int thread_set=50;
                         if(duration_min<service.getMintime()){
-                            thread_set= service.getChecktime()==0?(data.getQuantity() / (60/(duration_min)*2)):(int)(data.getQuantity()/2.6);
+                            thread_set= service.getChecktime()==0? (int) (data.getQuantity() / (60 / (duration_min) * 1.5)) :(int)(data.getQuantity()/2.6);
                             //thread_set= service.getChecktime()==0?(data.getQuantity() / (60/(duration_min))):(int)(data.getQuantity()/2.6);
                         }else{
-                            thread_set= service.getChecktime()==0?(data.getQuantity() / (60/(service.getMaxtime())*2)):(int)(data.getQuantity()/2.6);
+                            thread_set= service.getChecktime()==0? (int) (data.getQuantity() / (60 / (service.getMaxtime()) * 1.5)) :(int)(data.getQuantity()/2.6);
                             //thread_set= service.getChecktime()==0?(data.getQuantity() / (60/(service.getMaxtime()))):(int)(data.getQuantity()/2.6);
                         }
                         if (thread_set <= setting.getMaxthread()){
--- src/main/java/com/nts/awspremium/repositories/VideoViewRepository.java
+++ src/main/java/com/nts/awspremium/repositories/VideoViewRepository.java
@@ -43,12 +43,12 @@ public interface VideoViewRepository extends JpaRepository<VideoView,Long> {
 
     @Query(value = "select orderid from (select videoview.orderid,count(running) as total,maxthreads,valid,viewtotal,vieworder,speedup,threadset\n" +
             "                              from videoview left join historyview on historyview.orderid=videoview.orderid and running=1\n" +
-            "                                  group by orderid having total<maxthreads or  (((select bonus/100 from setting where id=1)+1)*vieworder-viewtotal>total*2 and threadset*3>total and speedup=1) ) as t",nativeQuery = true)
+            "                                  group by orderid having total<maxthreads or  (((select bonus/100 from setting where id=1)+1)*vieworder-viewtotal>total*2 and threadset*2>total and speedup=1) ) as t",nativeQuery = true)
     public List<String>  getListOrderSpeedTrueThreadONTEST();
 
     @Query(value = "select orderid from (select videoview.orderid,count(running) as total,maxthreads,valid,viewtotal,vieworder,speedup,threadset,timestart\n" +
             "                                        from videoview left join historyview on historyview.orderid=videoview.orderid and running=1 \n" +
-            "                                            group by orderid having (threadset*3>total and round((UNIX_TIMESTAMP()-timestart/1000)/60)>=90 ) ) as t",nativeQuery = true)
+            "                                            group by orderid having (threadset*3>total and round((UNIX_TIMESTAMP()-timestart/1000)/60)>=120 ) ) as t",nativeQuery = true)
     public List<String>  getListOrderSpeedTimeTrueThread();
 
     @Query(value = "select orderid from (select videoview.orderid,count(running) as total,maxthreads\n" +
