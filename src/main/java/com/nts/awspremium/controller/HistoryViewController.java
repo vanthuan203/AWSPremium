77d321eff96a68706ca8c33519c2293cfe1c1255
vocuc203 <nguyenthuan281294@gmail.com>
2024-10-17 13:19:04 +0700

    7/26/2024 update

--- src/main/java/com/nts/awspremium/controller/HistoryViewController.java
+++ src/main/java/com/nts/awspremium/controller/HistoryViewController.java
@@ -324,7 +324,7 @@ public class HistoryViewController {
         }
         Random ran = new Random();
         try {
-            //Thread.sleep(ran.nextInt(1000));
+            Thread.sleep(ran.nextInt(500));
 /*
             if(ran.nextInt(100)<60){
                 resp.put("status", "fail");
