3cbc7d0dde3099851b700dca8452114b5f83dfa1
vocuc203 <nguyenthuan281294@gmail.com>
2025-01-11 00:16:22 +0700

    7/26/2024 update

--- src/main/java/com/nts/awspremium/crons/RunCurrentCount.java
+++ src/main/java/com/nts/awspremium/crons/RunCurrentCount.java
@@ -27,7 +27,7 @@ public class RunCurrentCount {
                     while (true) {
                         try {
                             try {
-                                Thread.sleep(60000);
+                                Thread.sleep(15000);
                             } catch (InterruptedException e) {
                                 throw new RuntimeException(e);
                             }
