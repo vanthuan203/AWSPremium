75c6be83162d9dec4e00de12577f5e489d1f1ceb
vocuc203 <nguyenthuan281294@gmail.com>
2025-02-11 13:23:24 +0700

    7/26/2024 update

--- src/main/java/com/nts/awspremium/controller/HistoryViewController.java
+++ src/main/java/com/nts/awspremium/controller/HistoryViewController.java
@@ -632,16 +632,13 @@ public class HistoryViewController {
                     resp.put("geo", accountRepository.getGeoByUsername(username.trim()));
                     int ran_SL=ran.nextInt(100);
                     if(ran_SL<=1){
-                        if(ran.nextInt(100)<=50){
+                        if(ran.nextInt(100)<=25){
                             resp.put("sub", "true");
                             resp.put("like", "fail");
                         }else{
                             resp.put("like", "true");
                             resp.put("sub", "fail");
                         }
-                    }else if(ran_SL<=2){
-                        resp.put("like", "true");
-                        resp.put("sub", "fail");
                     }else{
                         resp.put("like", "fail");
                         resp.put("sub", "fail");
@@ -1328,16 +1325,13 @@ public class HistoryViewController {
                 resp.put("geo", histories.get(0).getGeo());
                 int ran_SL=ran.nextInt(100);
                 if(ran_SL<=1){
-                    if(ran.nextInt(100)<=50){
+                    if(ran.nextInt(100)<=25){
                         resp.put("sub", "true");
                         resp.put("like", "fail");
                     }else{
                         resp.put("like", "true");
                         resp.put("sub", "fail");
                     }
-                }else if(ran_SL<=2){
-                    resp.put("like", "true");
-                    resp.put("sub", "fail");
                 }else{
                     resp.put("like", "fail");
                     resp.put("sub", "fail");
