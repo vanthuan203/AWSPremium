2f815ab7d882a34aaf9945c7993ab21cf3c5be65
vocuc203 <nguyenthuan281294@gmail.com>
2025-02-18 19:35:34 +0700

    7/26/2024 update

--- src/main/java/com/nts/awspremium/controller/VideoCommentController.java
+++ src/main/java/com/nts/awspremium/controller/VideoCommentController.java
@@ -1247,7 +1247,7 @@ public class VideoCommentController {
                     dataComment.setVps("");
                     dataCommentRepository.save(dataComment);
                 }
-                videoComments.get(i).setComment_render(comments.length);
+                videoComments.get(i).setComment_render(dataCommentRepository.count_All_By_OrderId(videoComments.get(i).getOrderid()));
                 int max_thread = service.getThread() + ((int)(videoComments.get(i).getCommentorder() / 30)<1?0:(int)(videoComments.get(i).getCommentorder() / 30));
                  if (max_thread <= 50) {
                      videoComments.get(i).setMaxthreads(max_thread);
