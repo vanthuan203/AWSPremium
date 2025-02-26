ea51f9a1f194d6729d6afe5db98db177e29872a6
vocuc203 <nguyenthuan281294@gmail.com>
2025-02-26 14:00:45 +0700

    7/26/2024 update

--- src/main/java/com/nts/awspremium/controller/AccountViewController.java
+++ src/main/java/com/nts/awspremium/controller/AccountViewController.java
@@ -259,7 +259,7 @@ public class AccountViewController {
 
                         resp.put("status", "true");
                         resp.put("username", account.get(0).getUsername());
-                        resp.put("geo", account.get(0).getGeo());
+                        resp.put("geo", account.get(0).getGeo().replace("cmt-","").trim());
                         resp.put("password", account.get(0).getPassword());
                         resp.put("recover", account.get(0).getRecover());
                         resp.put("cookie", "");
@@ -354,7 +354,7 @@ public class AccountViewController {
 
                     resp.put("status", "true");
                     resp.put("username", accountbyVps.get(0).getUsername());
-                    resp.put("geo", accountbyVps.get(0).getGeo());
+                    resp.put("geo", accountbyVps.get(0).getGeo().replace("cmt-","").trim());
                     resp.put("recover", accountbyVps.get(0).getRecover());
                     resp.put("cookie", "");
                     resp.put("password", accountbyVps.get(0).getPassword());
