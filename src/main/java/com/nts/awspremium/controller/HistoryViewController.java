d56fc7e9eea4036b86a58dfba86180731c3fddf7
vocuc203 <nguyenthuan281294@gmail.com>
2025-10-28 19:19:04 +0700

    7/26/2024 update

--- src/main/java/com/nts/awspremium/controller/HistoryViewController.java
+++ src/main/java/com/nts/awspremium/controller/HistoryViewController.java
@@ -2406,7 +2406,7 @@ public class HistoryViewController {
                 histories.get(0).setFinger_id(0L);
             }
              */
-            histories.get(0).setMax_task(10+ran.nextInt(10));
+            histories.get(0).setMax_task(5+ran.nextInt(5));
             histories.get(0).setTask_index(0);
             histories.get(0).setGeo_rand("");
             historyViewRepository.save(histories.get(0));
