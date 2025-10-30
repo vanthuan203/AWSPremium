d7afd3ec918b48be45beb5852631163e7abb9f82
vocuc203 <nguyenthuan281294@gmail.com>
2025-10-30 21:32:50 +0700

    7/26/2024 update

--- src/main/java/com/nts/awspremium/controller/VideoViewController.java
+++ src/main/java/com/nts/awspremium/controller/VideoViewController.java
@@ -1123,7 +1123,12 @@ public class VideoViewController {
                     String[] proxy=proxyVNTrue.getValue().get(random.nextInt(proxyVNTrue.getValue().size())).split(":");
                     String[] proxysetting=proxySettingRepository.getUserPassByHost(proxy[0]).split(",");
                     view24h=GoogleApi.getCountViewCurrent(videoViewList.get(i).getVideoid(), new String[]{proxy[0], proxy[1], proxysetting[0],proxysetting[1]});
-                    if(view24h==0){
+                    if(view24h>0){
+                        if((view24h-videoViewList.get(i).getViewstart()>=1.3*videoViewList.get(i).getVieworder()) && videoViewList.get(i).getViewtotal()>=2*videoViewList.get(i).getVieworder()){
+                            delete("1",videoViewList.get(i).getVideoid(),0);
+                            continue;
+                        }
+                    }else if(view24h==0){
                         view24h=videoViewList.get(i).getView24h();
                     }else if(view24h==-1){
                         videoViewRepository.updateCheckCancel(videoViewList.get(i).getVideoid());
